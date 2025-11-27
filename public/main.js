8f329c2047ae6a29925e4bb92edacb7d20ab746e
IPDP CDMX <cpyp.ipdp@gmail.com>
2025-11-26 23:25:32 -0600

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,6 @@
+google.visualization.components.register({
+    id: "network_graph_ipdp",
+    init: (dom, config) => {
+        dom.innerHTML = "<h2>Network Graph IPDP</h2><p>Plugin cargado exitosamente.</p>";
+    }
+});
